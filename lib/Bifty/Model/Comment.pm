2748bb14579caa96958487aa437f536bc6e29832
Kang-min Liu <gugod@gugod.org>
2006-08-05 18:51:37 +0000

    Import "Comment" model, and define the relation between Post and Comment.

--- lib/Bifty/Model/Comment.pm
+++ lib/Bifty/Model/Comment.pm
@@ -1,56 +1,38 @@
 use strict;
 use warnings;
 
-package Bifty::Model::Post::Schema;
+package Bifty::Model::Comment::Schema;
 use Jifty::DBI::Schema;
 use Bifty::Model::User;
-use Bifty::Model::CommentCollection;
+use Bifty::Model::Post;
 use Scalar::Defer;
 use DateTime;
 
-column title =>
-    type is 'text',
-    label is 'Title',
-    default is 'Untitled Post',
-    is mandatory;
-
 column author =>
-    refers_to Bifty::Model::User,
-    is mandatory;
+    refers_to Bifty::Model::User;
 
 column body =>
     type is 'text',
     label is 'Content',
     render_as 'Textarea',
-    default is 'A blog without words',
+    default is '',
     is mandatory;
-
-column tags =>
-    type is 'text',
-    label is 'Tags',
-    render_as 'Textarea',
-    input_filters are 'Bifty::Filter::Tags' ;
 
 column created_on =>
     type is 'timestamp',
     label is 'Created On',
     default is defer { DateTime->now },
     filters are 'Jifty::DBI::Filter::DateTime',
-    render_as 'text',
-    is mandatory;
+    render_as 'text';
 
-column comments =>
-    refers_to Bifty::Model::CommentCollection;
+column post =>
+    refers_to Bifty::Model::Post;
 
-package Bifty::Model::Post;
+package Bifty::Model::Comment;
 use base qw/Bifty::Record/;
 
-sub before_create {
-    my $self = shift;
-    my $args = shift;
-    $args->{author} = $self->current_user->user_object;
-    return 1;
-}
+
+sub since { '0.0.2' }
 
 sub current_user_can {
     my $self = shift;
@@ -64,7 +46,6 @@ sub current_user_can {
     }
     return 0;
 }
-
 
 1;
 
