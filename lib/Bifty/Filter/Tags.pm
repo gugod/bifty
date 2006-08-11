9d853c4ea4ba20e22a1ca13fbb2f5f4a8b75c86f
Kang-min Liu <gugod@gugod.org>
2006-08-11 14:24:54 +0000

    sort+uniq tags in the filter.

--- lib/Bifty/Filter/Tags.pm
+++ lib/Bifty/Filter/Tags.pm
@@ -2,17 +2,16 @@ package Bifty::Filter::Tags;
 use strict;
 use warnings;
 use base 'Jifty::DBI::Filter';
+use List::MoreUtils qw(uniq);
 
 sub encode {
     my $self = shift;
     my $value_ref = $self->value_ref;
     return unless $$value_ref;
-    $$value_ref = join " ", sort split /\W+/, lc($$value_ref) ;
+    $$value_ref = join " ", sort+uniq(split /\W+/, lc($$value_ref) );
     return 1;
 }
 
-sub decode {
-    return;
-}
+sub decode { }
 
 1;
