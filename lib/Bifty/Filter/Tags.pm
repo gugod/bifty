65e041504dca1b2303cd8bd9d2f3224b8ae09a73
Kang-min Liu <gugod@gugod.org>
2006-08-11 14:24:06 +0000

    Make it clearer here that tags are consists of \w, and all \W seperate tags, and will be destroyed in db.

--- lib/Bifty/Filter/Tags.pm
+++ lib/Bifty/Filter/Tags.pm
@@ -7,7 +7,7 @@ sub encode {
     my $self = shift;
     my $value_ref = $self->value_ref;
     return unless $$value_ref;
-    $$value_ref = join " ", sort split /\s+/, lc($$value_ref) ;
+    $$value_ref = join " ", sort split /\W+/, lc($$value_ref) ;
     return 1;
 }
 
