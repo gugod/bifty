5fd32dc4613a06302e71d6971098f83d8867f557
Kang-min Liu <gugod@gugod.org>
2006-10-14 15:52:16 +0000

    r9453@GOP:  gugod | 2006-10-14 23:50:43 +0800  Add a auto-resize login to the content textarea when posting blogs.

---
+++ share/web/static/js/app.js
@@ -0,0 +1,23 @@
+/* Put your application's custom JS here... */
+
+Bifty = {}
+
+Bifty.Textarea = {
+    'min_height': 80
+}
+
+Bifty.textarea_resize = function(element) {
+    var bottom = window.innerHeight - 100;
+    var top = element.offsetTop;
+    var h = bottom - top;
+    if ( h > Bifty.Textarea.min_height ) {
+        element.style.height = h + 'px';
+    } 
+}
+
+Bifty.textarea_setup = function(element) {
+    Bifty.textarea_resize( element );
+    window.onresize = function() {
+        Bifty.textarea_resize( element );
+    };
+}
