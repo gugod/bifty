3ed9f602e8e20b536571445006461da939424d4f
Kang-min Liu <gugod@gugod.org>
2006-12-06 22:11:35 +0000

    r12555@GOP:  gugod | 2006-12-06 20:37:02 +0800  Widget.TagCloud is in bifty game.

--- lib/Bifty.pm
+++ lib/Bifty.pm
@@ -5,7 +5,10 @@ use warnings;
 
 sub start {
     Jifty->web->javascript_libs(
-        [ @{ Jifty->web->javascript_libs }, "yui/tabview.js" ] );
+        [   @{ Jifty->web->javascript_libs }, "yui/tabview.js",
+            "Widget/TagCloud.js"
+        ]
+    );
 }
 
 1;
