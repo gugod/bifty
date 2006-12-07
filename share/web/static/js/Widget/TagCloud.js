66596a6db704e3f7cce229298dec4c93a76a2a1b
Kang-min Liu <gugod@gugod.org>
2006-12-07 17:14:11 +0000

    r12588@GOP:  gugod | 2006-12-08 01:11:48 +0800  merge Widget.TagCloud's IE fix.

--- share/web/static/js/Widget/TagCloud.js
+++ share/web/static/js/Widget/TagCloud.js
@@ -6,7 +6,7 @@ Widget.TagCloud = function(param){
     return this;
 }
 
-Widget.TagCloud.VERSION = '0.01'
+Widget.TagCloud.VERSION = '0.02'
 Widget.TagCloud.EXPORT = []
 Widget.TagCloud.EXPORT_OK = []
 Widget.TagCloud.EXPORT_TAGS = {}
@@ -25,7 +25,7 @@ Widget.TagCloud.prototype = (function(){return {
     },
     create: function() {
         var d = this._data.sort(function(a, b) {return a.count - b.count})
-        var max = d[data.length-1].count
+        var max = d[d.length-1].count
         var factor = 36/Math.log(max)
 
         this.data(this._data.sort(function(a, b) {
@@ -33,30 +33,30 @@ Widget.TagCloud.prototype = (function(){return {
             var tb = b.tag.toLowerCase()
             return ((ta > tb) ? 1 : (ta < tb) ? -1 : 0 )}))
 
+        var levels = new Array()
         this.div = (function(data){
             var d = document.createElement("div")
             d.setAttribute("class", "jsan-widget-tagcloud")
+            d.style.lineHeight = "1em"
+            d.style.textAlign = "center"
             for (var i = 0; i < data.length ; i++) {
                 var a = document.createElement("a")
                 a.setAttribute("href", data[i].url)
                 a.appendChild(document.createTextNode(data[i].tag))
                 var s = document.createElement("span")
-                var level = Math.floor(Math.log(data[i].count) * factor);
+                var level = Math.floor(Math.log(data[i].count) * factor)
                 s.setAttribute("class", "jsan-widget-tagcloud" + level)
+                a.style.fontSize = level + "pt"
+                a.style.textDecoration = "none"
+                levels.push(level)
                 s.appendChild(a)
                 d.appendChild(s)
                 d.appendChild(document.createTextNode(" "))
             }
             return d
         })(this._data)
+        this.levels = levels
 
-        this.style = (function(css){
-            var s = document.createElement("style")
-            s.setAttribute("type", "text/css")
-            s.appendChild(document.createTextNode(css))
-            return s
-        })(this.css())
- 
         this.state.created = 1
         return this
     },
@@ -67,30 +67,15 @@ Widget.TagCloud.prototype = (function(){return {
            param.parentNode = document.body
         
         param.parentNode.appendChild(this.div)
-        var head = ( document.getElementsByTagName("head") )[0]
-        head.appendChild(this.style)
         this.state.shown = 1
     },
     hide: function() {
         if ( this.state.shown ) {
             document.body.removeChild(this.div)
-            var head = ( document.getElementsByTagName("head") )[0]
-            head.removeChild(this.style)
         }
     },
     add: function(tag, url, count) {
         this._data.push({tag: tag, url: url, count: count })
-    },
-    css: function() {
-        var L = ".jsan-widget-tagcloud {line-height:1em;text-align:center;}\n.jsan-widget-tagcloud a {text-decoration: none;}\nYIELD\n"
-        var T = ".jsan-widget-tagcloudLEVEL {font-size: SIZEpx;}\n"
-
-        var rule = '';
-        for(var i = 0; i < this._data.length; i++) {
-            rule += T.replace(/LEVEL/, i).replace(/SIZE/, i + 12)
-        }
-        
-        return L.replace(/YIELD/, rule)
     },
     data: function(data) {
         if ( data ) {
